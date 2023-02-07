62eef17a27b579956f48a28964622304e2153399
Amin <amin.yassin@live.com>
2023-02-07 08:01:41 +0100

    Added component for work experience

--- Portfolio/src/App.vue
+++ Portfolio/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <InfoComp myName="Amin Yassin" />
+  <ExpComponent></ExpComponent>
 </template>
 
 <script>
+import ExpComponent from './components/ExpComponent.vue';
 import HelloWorld from './components/HelloWorld.vue'
 import InfoComp from './components/InfoComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    InfoComp
+    InfoComp,
+    ExpComponent
   }
 }
 </script>
---
+++ Portfolio/src/components/ExpComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <h3 id="avsnitt">Arbetslivserfarenhet</h3>
+    <div>
+        <ol>
+            <li id="workItems" v-for="item in workItems">
+                {{ item.role }} - {{ item.company }} - {{ item.date }}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'exp',
+    data() {
+        return {
+            workItems: [
+                {
+                    role: 'Systemutvecklare och förvaltare',
+                    company: 'Formpipe Software AB',
+                    date: '2019-03 - Pågående'
+                },
+                {
+                    role: 'Teknisk Support',
+                    company: 'Barkfors Fleet AB',
+                    date: '2020-05 - 2020-11'
+                }
+            ],
+
+            responsibilitiesFpSwe: [
+                'Säkrar kodfunktionalitet i samband med kodmigrering vid uppgraderingsprojekt av ärendehanteringssytem i agilt team.',
+                'Åtgärdar buggar i användargränssnitt (UI) och dess logik (Backend).',
+                'Genomför felsökningar i databaser och tar fram databasskript för kunders varierande ändamål.',
+                'Implementerar nya system och integrerar dessa i kunders befintliga ärendehanteringssystem.',
+                'Samarbetar med kollegor och internationella aktörer vid felsökningar av kunders system.'
+            ],
+            responsibilitiesFpEng: [
+                'Delivering bug fixes and user-control customizations while upgrading EDM systems using ASP.NET and updating SQL Server procedures.',
+                'Implementing and launching new systems on stakeholders’ environments.',
+                'Investigating root causes of reported incidents and problems and collaborating with colleagues and vendors.'
+            ]
+        }
+    }
+};
+
+</script>
+
+<style>
+#avsnitt {
+    float: left;
+    margin: 10px;
+}
+
+#workItems {
+    font-weight: bold;
+    list-style-type: none;
+    margin-top: 20px;
+}
+</style>
