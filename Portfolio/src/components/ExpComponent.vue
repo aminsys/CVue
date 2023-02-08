2a6a6ced7ea8dd5d2eb36c9f4b255d1dee102423
Amin <amin.yassin@live.com>
2023-02-08 07:27:05 +0100

    Added responsibilities and fixed some styling

--- Portfolio/src/components/ExpComponent.vue
+++ Portfolio/src/components/ExpComponent.vue
@@ -4,6 +4,9 @@
         <ol>
             <li id="workItems" v-for="item in workItems">
                 {{ item.role }} - {{ item.company }} - {{ item.date }}
+            <li id="responsibilities" v-for="r in item.responsibilities">
+                {{ r }}
+            </li>
             </li>
         </ol>
     </div>
@@ -18,27 +21,25 @@ export default {
                 {
                     role: 'Systemutvecklare och förvaltare',
                     company: 'Formpipe Software AB',
-                    date: '2019-03 - Pågående'
+                    date: '2019-03 - Pågående',
+                    responsibilities: [
+                        'Säkrar kodfunktionalitet i samband med kodmigrering vid uppgraderingsprojekt av ärendehanteringssytem i agilt team.',
+                        'Åtgärdar buggar i användargränssnitt (UI) och dess logik (Backend).',
+                        'Genomför felsökningar i databaser och tar fram databasskript för kunders varierande ändamål.',
+                        'Implementerar nya system och integrerar dessa i kunders befintliga ärendehanteringssystem.',
+                        'Samarbetar med kollegor och internationella aktörer vid felsökningar av kunders system.'
+                    ],
                 },
                 {
                     role: 'Teknisk Support',
                     company: 'Barkfors Fleet AB',
-                    date: '2020-05 - 2020-11'
+                    date: '2020-05 - 2020-11',
+                    responsibilities: [
+                        'Utfört tekniska felsökningar av systemflöden, analys av EDI integrationer, och databasrättningar i SQL Server för diverse kunder.',
+                        'Gett vägledning till användare av transport- och faktureringssystem.'
+                    ]
                 }
             ],
-
-            responsibilitiesFpSwe: [
-                'Säkrar kodfunktionalitet i samband med kodmigrering vid uppgraderingsprojekt av ärendehanteringssytem i agilt team.',
-                'Åtgärdar buggar i användargränssnitt (UI) och dess logik (Backend).',
-                'Genomför felsökningar i databaser och tar fram databasskript för kunders varierande ändamål.',
-                'Implementerar nya system och integrerar dessa i kunders befintliga ärendehanteringssystem.',
-                'Samarbetar med kollegor och internationella aktörer vid felsökningar av kunders system.'
-            ],
-            responsibilitiesFpEng: [
-                'Delivering bug fixes and user-control customizations while upgrading EDM systems using ASP.NET and updating SQL Server procedures.',
-                'Implementing and launching new systems on stakeholders’ environments.',
-                'Investigating root causes of reported incidents and problems and collaborating with colleagues and vendors.'
-            ]
         }
     }
 };
@@ -55,5 +56,10 @@ export default {
     font-weight: bold;
     list-style-type: none;
     margin-top: 20px;
+}
+
+#responsibilities {
+    font-weight: normal;
+    list-style-type: square;
 }
 </style>
