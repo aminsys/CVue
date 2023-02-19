62b12789382322da0c8eb3fdfe5ea0d99ab7928f
Amin <amin.yassin@live.com>
2023-02-19 10:31:54 +0100

    changed to type=text/javascript in index.html

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-import { defineConfig } from 'vite'
-import { vue } from '@vitejs/plugin-vue'
-
-export default defineConfig({
-    base: '/CVue/',
-    plugin: [vue()]
-})
-
-/*
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? "/CVue/" : "/",
-}; */
+};
